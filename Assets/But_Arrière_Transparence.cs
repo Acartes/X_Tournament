df84063f64a9a021cc442ec1db4ab58c0b39dcb0
Unknown <oncheonche@ymail.com>
2018-01-27 21:54:34 +0100

    Le but arrière devient transparent quand un objet passe en dessous.
    + appliqué les prefabs Canvas et But_Arriere

---
+++ Assets/But_Arrière_Transparence.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class But_Arrière_Transparence : MonoBehaviour {
+
+    SpriteRenderer img;
+    short compteur;
+    public Color transparencyAlpha;
+    public Color opacityAlpha;
+
+    // Use this for initialization
+    void Start () {
+        img = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        Debug.Log(compteur);
+        if(compteur > 0)
+        {
+            img.color = transparencyAlpha;
+        }
+        else
+        {
+            img.color = opacityAlpha;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Personnage" || other.tag == "Ballon")
+            ++compteur;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Personnage" || other.tag == "Ballon")
+            --compteur;
+    }
+}
