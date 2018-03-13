e63a21c842cc96319e50848e8e7f59ad20cc05bd
Unknown <oncheonche@ymail.com>
2018-03-13 22:37:15 +0100

    (pas 100% sûr) le delai devrait marcher

--- Assets/Script/Event/HoverEvent.cs
+++ Assets/Script/Event/HoverEvent.cs
@@ -34,11 +34,10 @@ public class HoverEvent : NetworkBehaviour
 
   void OnMouseOver()
   {
-  
-    if (RpcFunctions.Instance.localId == 0 && TurnManager.Instance.currentPlayer == Player.Blue)
-      return;
-    if (RpcFunctions.Instance.localId == 1 && TurnManager.Instance.currentPlayer == Player.Red)
-      return;
+
+    if ((RpcFunctions.Instance.localId == 0 && TurnManager.Instance.currentPlayer == Player.Blue)
+      || (RpcFunctions.Instance.localId == 1 && TurnManager.Instance.currentPlayer == Player.Red))
+    return;
 
     if (!enabled || !LoadingManager.Instance.isGameReady())
       return;
@@ -55,16 +54,14 @@ public class HoverEvent : NetworkBehaviour
     if (GetComponent<CaseData>().ballon != null)
       hoveredBallon = GetComponent<CaseData>().ballon.name;
 
-    RpcFunctions.Instance.CmdHoverEvent(hoveredCase, hoveredPersonnage, hoveredBallon);
-
-       
-  }
+    RpcFunctions.Instance.CmdSendHoverEvent(hoveredCase, hoveredPersonnage, hoveredBallon);
+    }
 
   void OnMouseExit()
   {
     if (!enabled || !LoadingManager.Instance.isGameReady())
         {
-            RpcFunctions.Instance.CmdHoverEvent("null", "null", "null");
+            RpcFunctions.Instance.CmdSendHoverEvent("null", "null", "null");
         }
         return;
   }
--- Assets/Script/Réseau/RpcFunctions.cs
+++ Assets/Script/Réseau/RpcFunctions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -7,11 +8,11 @@ using Prototype.NetworkLobby;
 
 public class RpcFunctions : NetworkBehaviour {
 
-
-
     public static RpcFunctions Instance;
 
     public int localId;
+
+    public bool validatedCommand = true;
 
     public override void OnStartLocalPlayer()
     {
@@ -33,33 +34,66 @@ public class RpcFunctions : NetworkBehaviour {
     {
         TurnManager.Instance.RpcFirstTurn();
     }
-/*    [Command]
-    public void CmdPlacePerso(string hoveredCase, float offsetY, string selectedPersonnage)
-    {
-        PlacementBehaviour.Instance.RpcCreatePersoPlacement(hoveredCase, offsetY, selectedPersonnage);
-    }*/
- /* [Command]
-  public void CmdDeplacement(float offsetY, string selectedPersonnage)
-    {
-    if (localId == 0 && isLocalPlayer)
-      {
-        MoveBehaviour.Instance.RpcDeplacement(offsetY, selectedPersonnage);
-      }
-    }*/
+    /*    [Command]
+        public void CmdPlacePerso(string hoveredCase, float offsetY, string selectedPersonnage)
+        {
+            PlacementBehaviour.Instance.RpcCreatePersoPlacement(hoveredCase, offsetY, selectedPersonnage);
+        }*/
+    /* [Command]
+     public void CmdDeplacement(float offsetY, string selectedPersonnage)
+       {
+       if (localId == 0 && isLocalPlayer)
+         {
+           MoveBehaviour.Instance.RpcDeplacement(offsetY, selectedPersonnage);
+         }
+       }*/
 
-  [Command]
-    public void CmdIsAllGameReady () {
-      LoadingManager.Instance.RpcIsAllGameReady();
-    }
-
-  [Command]
-    public void CmdSpawnPlayers () {
-      RosterManager.Instance.RpcSpawnPlayers();
+    [Command]
+    public void CmdIsAllGameReady() {
+        LoadingManager.Instance.RpcIsAllGameReady();
     }
 
     [Command]
-    public void CmdHoverEvent (string hoveredCase, string hoveredPersonnage, string hoveredBallon) {
-      EventManager.Instance.RpcHoverEvent (hoveredCase, hoveredPersonnage, hoveredBallon);
+    public void CmdSpawnPlayers() {
+        RosterManager.Instance.RpcSpawnPlayers();
+    }
+
+
+    [Command]
+    public void CmdSendHoverEvent(string hoveredCase, string hoveredPersonnage, string hoveredBallon)
+    {
+        Debug.Log("sending event");
+        EventManager.Instance.RpcReceiveHoverEvent(hoveredCase, hoveredPersonnage, hoveredBallon);
+        // Au cas où il y a un nouveau hover, la coroutine se reset
+        StopAllCoroutines();
+
+        // On lance la coroutine a qui on a donne la fonction de validation
+        StartCoroutine(WaitForHoverEventValidation(hoveredCase, hoveredPersonnage, hoveredBallon));
+    }
+
+    IEnumerator WaitForHoverEventValidation(string hoveredCase, string hoveredPersonnage, string hoveredBallon)
+    {
+        validatedCommand = false;
+        if (validatedCommand == false)
+        {
+            Debug.Log("wait for validation of event");
+            // si la fonction n'a pas été validé au bout de 0.X secondes, on relance
+            yield return new WaitForSeconds(0.5f);
+        }
+        if (validatedCommand == false)
+        {
+            Debug.Log("event not validated, resend...");
+            CmdSendHoverEvent(hoveredCase, hoveredPersonnage, hoveredBallon);
+        }
+        StopAllCoroutines();
+    }
+
+
+
+    [Command]
+    public void CmdValidateHoverEvent(string hoveredCase, string hoveredPersonnage, string hoveredBallon)
+    {
+        EventManager.Instance.RpcValidateHoverEvent(hoveredCase, hoveredPersonnage, hoveredBallon);
     }
 
     [Command]
