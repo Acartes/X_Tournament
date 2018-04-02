a130b70632eb7ba3fa7326e2cf140dd06dd5d502
Unknown <oncheonche@ymail.com>
2018-04-02 22:36:44 +0200

    [UI] Portraits miroir

--- Assets/Script/UI/infoPersosPortrait.cs
+++ Assets/Script/UI/infoPersosPortrait.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Networking;
 
-public class infoPersoPortraits : NetworkBehaviour
+public class infoPersosPortrait : NetworkBehaviour
 {
 
     public PortraitInteractive MainPortrait;
