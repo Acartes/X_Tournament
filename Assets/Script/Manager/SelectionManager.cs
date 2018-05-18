46751a257c545ba1c0ac52643e69692ecdf0372b
JulienGuenard <julienguenard01@gmail.com>
2018-05-18 11:47:49 +0200

    Ajout de la nouvelle police et des barres

--- Assets/Script/Manager/SelectionManager.cs
+++ Assets/Script/Manager/SelectionManager.cs
@@ -87,9 +87,12 @@ public class SelectionManager : NetworkBehaviour
           }
         break;
       }
-    InfoPerso.Instance.stats.changePm(Instance.selectedPersonnage.actualPointMovement, Instance.selectedPersonnage.maxPointMovement);
-    InfoPerso.Instance.stats.changePr(Instance.selectedPersonnage.actualPointResistance, Instance.selectedPersonnage.maxPointResistance);
-    InfoPerso.Instance.stats.changePo(Instance.selectedPersonnage.shotStrenght, Instance.selectedPersonnage.shotStrenght);
+    if (selectedPersonnage != null)
+      {
+        InfoPerso.Instance.stats.changePm(Instance.selectedPersonnage.actualPointMovement, Instance.selectedPersonnage.maxPointMovement);
+        InfoPerso.Instance.stats.changePr(Instance.selectedPersonnage.actualPointResistance, Instance.selectedPersonnage.maxPointResistance);
+        InfoPerso.Instance.stats.changePo(Instance.selectedPersonnage.shotStrenght, Instance.selectedPersonnage.shotStrenght);
+      }
   }
 
   void OnChangeTurn(object sender, PlayerArgs e)
