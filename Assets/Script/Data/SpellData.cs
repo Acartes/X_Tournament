c76b3046a0e2bd06b42681a8eda48b4f3372d600
JulienGuenard <julienguenard01@gmail.com>
2018-05-22 17:14:25 +0200

    lul2

--- Assets/Script/Data/SpellData.cs
+++ Assets/Script/Data/SpellData.cs
@@ -69,9 +69,9 @@ public class SpellData : NetworkBehaviour
   {
     yield return new WaitForEndOfFrame();
     while (TurnManager.Instance == null)
-    {
-      yield return null;
-    }
+      {
+        yield return null;
+      }
     Init();
   }
 
@@ -87,16 +87,16 @@ public class SpellData : NetworkBehaviour
   public void ShowAllFeedbacks()
   {
     foreach (CaseData obj in CaseManager.listAllCase)
-    {
-      obj.ChangeStatut(Statut.None, Statut.atAoE);
-      obj.ChangeStatut(Statut.None, Statut.atPush);
-    }
+      {
+        obj.ChangeStatut(Statut.None, Statut.atAoE);
+        obj.ChangeStatut(Statut.None, Statut.atPush);
+      }
 
     if (!targetList.Contains(HoverManager.Instance.hoveredCase))
-    {
-      BeforeFeedbackManager.Instance.HidePrediction();
-      return;
-    }
+      {
+        BeforeFeedbackManager.Instance.HidePrediction();
+        return;
+      }
 
     ShowAreaOfEffect();
     ShowPushEffect();
@@ -113,72 +113,71 @@ public class SpellData : NetworkBehaviour
 
     // get des cases
     if (isLinear)
-    {
-      for (int i = 0; i < range + 1; i++)
       {
-        if (selectedCase.GetCaseRelativeCoordinate(i, 0) != null)
-          list.Add(selectedCase.GetCaseRelativeCoordinate(i, 0));
+        for (int i = 0; i < range + 1; i++)
+          {
+            if (selectedCase.GetCaseRelativeCoordinate(i, 0) != null)
+              list.Add(selectedCase.GetCaseRelativeCoordinate(i, 0));
 
-        if (selectedCase.GetCaseRelativeCoordinate(-i, 0) != null)
-          list.Add(selectedCase.GetCaseRelativeCoordinate(-i, 0));
+            if (selectedCase.GetCaseRelativeCoordinate(-i, 0) != null)
+              list.Add(selectedCase.GetCaseRelativeCoordinate(-i, 0));
 
-        if (selectedCase.GetCaseRelativeCoordinate(0, i) != null)
-          list.Add(selectedCase.GetCaseRelativeCoordinate(0, i));
+            if (selectedCase.GetCaseRelativeCoordinate(0, i) != null)
+              list.Add(selectedCase.GetCaseRelativeCoordinate(0, i));
 
-        if (selectedCase.GetCaseRelativeCoordinate(0, -i) != null)
-          list.Add(selectedCase.GetCaseRelativeCoordinate(0, -i));
-      }
-    }
-    else
-    {
-      for (int i = 0; i < range; i++)
+            if (selectedCase.GetCaseRelativeCoordinate(0, -i) != null)
+              list.Add(selectedCase.GetCaseRelativeCoordinate(0, -i));
+          }
+      } else
       {
-        foreach (CaseData obj in list)
-        {
-          if (obj.GetBottomLeftCase() != null)
-            list2.Add(obj.GetBottomLeftCase());
+        for (int i = 0; i < range; i++)
+          {
+            foreach (CaseData obj in list)
+              {
+                if (obj.GetBottomLeftCase() != null)
+                  list2.Add(obj.GetBottomLeftCase());
 
-          if (obj.GetBottomRightCase() != null)
-            list2.Add(obj.GetBottomRightCase());
+                if (obj.GetBottomRightCase() != null)
+                  list2.Add(obj.GetBottomRightCase());
 
-          if (obj.GetTopLeftCase() != null)
-            list2.Add(obj.GetTopLeftCase());
+                if (obj.GetTopLeftCase() != null)
+                  list2.Add(obj.GetTopLeftCase());
 
-          if (obj.GetTopRightCase() != null)
-            list2.Add(obj.GetTopRightCase());
-        }
-        list.AddRange(list2);
-        list2.Clear();
+                if (obj.GetTopRightCase() != null)
+                  list2.Add(obj.GetTopRightCase());
+              }
+            list.AddRange(list2);
+            list2.Clear();
+          }
+        list.Remove(selectedCase);
       }
-      list.Remove(selectedCase);
-    }
     rangeList.AddRange(list);
     foreach (CaseData obj in rangeList)
-    {
-      if (obj != null)
       {
-        obj.ChangeStatut(Statut.atRange);
+        if (obj != null)
+          {
+            obj.ChangeStatut(Statut.atRange);
+          }
       }
-    }
   }
 
   bool CheckRange()
   {
     foreach (CaseData obj in rangeList)
-    {
-      if (obj != null)
-        obj.ChangeStatut(Statut.atRange);
-    }
+      {
+        if (obj != null)
+          obj.ChangeStatut(Statut.atRange);
+      }
     return true;
   }
 
   bool CheckTarget()
   {
     foreach (CaseData obj in targetList)
-    {
-      if (obj != null)
-        obj.ChangeStatut(Statut.canTarget);
-    }
+      {
+        if (obj != null)
+          obj.ChangeStatut(Statut.canTarget);
+      }
     return true;
   }
 
@@ -188,36 +187,36 @@ public class SpellData : NetworkBehaviour
     CaseData hoveredCase = HoverManager.Instance.hoveredCase;
     targetList.Clear();
     foreach (CaseData obj in rangeList)
-    {
-      if ((ObjectType.Invoc & allowedTarget) == ObjectType.Invoc)
+      {
+        if ((ObjectType.Invoc & allowedTarget) == ObjectType.Invoc)
         if (obj.summonData != null && !targetList.Contains(obj))
           targetList.Add(obj);
 
-      if ((ObjectType.AllyPerso & allowedTarget) == ObjectType.AllyPerso)
+        if ((ObjectType.AllyPerso & allowedTarget) == ObjectType.AllyPerso)
         if (obj.personnageData != null && obj.personnageData.owner == GameManager.Instance.currentPlayer && !targetList.Contains(obj))
           targetList.Add(obj);
 
-      if ((ObjectType.EnemyPerso & allowedTarget) == ObjectType.EnemyPerso)
+        if ((ObjectType.EnemyPerso & allowedTarget) == ObjectType.EnemyPerso)
         if (obj.personnageData != null && obj.personnageData.owner != GameManager.Instance.currentPlayer && !targetList.Contains(obj))
           targetList.Add(obj);
 
-      if ((ObjectType.Ballon & allowedTarget) == ObjectType.Ballon)
+        if ((ObjectType.Ballon & allowedTarget) == ObjectType.Ballon)
         if (obj.ballon != null && !targetList.Contains(obj))
           targetList.Add(obj);
 
-      if ((ObjectType.EmptyCase & allowedTarget) == ObjectType.EmptyCase)
+        if ((ObjectType.EmptyCase & allowedTarget) == ObjectType.EmptyCase)
         if (obj.casePathfinding == PathfindingCase.Walkable && !targetList.Contains(obj) && obj.summonData == null)
           targetList.Add(obj);
 
-      if (!((ObjectType.Self & allowedTarget) == ObjectType.Self))
+        if (!((ObjectType.Self & allowedTarget) == ObjectType.Self))
         if (obj == SelectionManager.Instance.selectedCase && targetList.Contains(obj))
           targetList.Remove(obj);
-    }
+      }
     foreach (CaseData obj in targetList)
-    {
-      if (obj != null)
-        obj.ChangeStatut(Statut.canTarget);
-    }
+      {
+        if (obj != null)
+          obj.ChangeStatut(Statut.canTarget);
+      }
   }
 
   /// <summary>Montre l'AoE du sort avant de le lancer</summary>
@@ -228,42 +227,42 @@ public class SpellData : NetworkBehaviour
     ShowPushEffect();
 
     foreach (CaseData obj in CaseManager.Instance.GetAllCaseWithStatut(Statut.atAoE))
-    {
-      obj.ChangeStatut(Statut.None, Statut.atAoE);
-    }
+      {
+        obj.ChangeStatut(Statut.None, Statut.atAoE);
+      }
 
     int AoE = areaOfEffect;
     List<CaseData> list = new List<CaseData>();
     List<CaseData> list2 = new List<CaseData>();
     list.Add(hoveredCase);
     for (int i = 0; i < AoE; i++)
-    {
-      foreach (CaseData obj in list)
       {
-        if (obj.GetBottomLeftCase() != null)
-          list2.Add(obj.GetBottomLeftCase());
+        foreach (CaseData obj in list)
+          {
+            if (obj.GetBottomLeftCase() != null)
+              list2.Add(obj.GetBottomLeftCase());
 
-        if (obj.GetBottomRightCase() != null)
-          list2.Add(obj.GetBottomRightCase());
+            if (obj.GetBottomRightCase() != null)
+              list2.Add(obj.GetBottomRightCase());
 
-        if (obj.GetTopLeftCase() != null)
-          list2.Add(obj.GetTopLeftCase());
+            if (obj.GetTopLeftCase() != null)
+              list2.Add(obj.GetTopLeftCase());
 
-        if (obj.GetTopRightCase() != null)
-          list2.Add(obj.GetTopRightCase());
+            if (obj.GetTopRightCase() != null)
+              list2.Add(obj.GetTopRightCase());
+          }
+        list.AddRange(list2);
+        list2.Clear();
       }
-      list.AddRange(list2);
-      list2.Clear();
-    }
 
     if (AoEList.Count != 0)
       AoEList.Clear();
 
     AoEList.AddRange(list);
     foreach (CaseData obj in AoEList)
-    {
-      obj.ChangeStatut(Statut.atAoE);
-    }
+      {
+        obj.ChangeStatut(Statut.atAoE);
+      }
   }
 
   /// <summary>Montre à quelle portée les personnages vont être projetés avant de le lancer</summary>
@@ -273,23 +272,22 @@ public class SpellData : NetworkBehaviour
       return;
 
     if (HoverManager.Instance.hoveredPersonnage)
-    {
-      PersoData persoAfflicted = HoverManager.Instance.hoveredPersonnage;
-      PushBehaviour.Instance.PushCheck(persoAfflicted.gameObject, pushValue, persoAfflicted.persoCase, pushType, pushDirection);
-      if (persoAfflicted.persoCase != PushBehaviour.Instance.caseFinalShow)
-        BeforeFeedbackManager.Instance.PredictDeplacement(persoAfflicted.gameObject, PushBehaviour.Instance.caseFinalShow);
-      else
-        BeforeFeedbackManager.Instance.HidePrediction();
-    }
-    else if (HoverManager.Instance.hoveredBallon)
-    {
-      BallonData ballonAfflicted = HoverManager.Instance.hoveredBallon;
-      PushBehaviour.Instance.PushCheck(ballonAfflicted.gameObject, pushValue, ballonAfflicted.ballonCase, pushType, pushDirection);
-      if (ballonAfflicted.ballonCase != PushBehaviour.Instance.caseFinalShow)
-        BeforeFeedbackManager.Instance.PredictDeplacement(ballonAfflicted.gameObject, PushBehaviour.Instance.caseFinalShow);
-      else
-        BeforeFeedbackManager.Instance.HidePrediction();
-    }
+      {
+        PersoData persoAfflicted = HoverManager.Instance.hoveredPersonnage;
+        PushBehaviour.Instance.PushCheck(persoAfflicted.gameObject, pushValue, persoAfflicted.persoCase, pushType, pushDirection);
+        if (persoAfflicted.persoCase != PushBehaviour.Instance.caseFinalShow)
+          BeforeFeedbackManager.Instance.PredictDeplacement(persoAfflicted.gameObject, PushBehaviour.Instance.caseFinalShow);
+        else
+          BeforeFeedbackManager.Instance.HidePrediction();
+      } else if (HoverManager.Instance.hoveredBallon)
+      {
+        BallonData ballonAfflicted = HoverManager.Instance.hoveredBallon;
+        PushBehaviour.Instance.PushCheck(ballonAfflicted.gameObject, pushValue, ballonAfflicted.ballonCase, pushType, pushDirection);
+        if (ballonAfflicted.ballonCase != PushBehaviour.Instance.caseFinalShow)
+          BeforeFeedbackManager.Instance.PredictDeplacement(ballonAfflicted.gameObject, PushBehaviour.Instance.caseFinalShow);
+        else
+          BeforeFeedbackManager.Instance.HidePrediction();
+      }
   }
 
 
@@ -298,17 +296,17 @@ public class SpellData : NetworkBehaviour
   {
     CaseData hoveredCase = HoverManager.Instance.hoveredCase;
     if (summonedObj != null)
-    {
-      if (SummonManager.Instance.lastSummonInstancied == null)
       {
-        SummonManager.Instance.lastSummonInstancied = (SummonData)Instantiate(summonedObj, hoveredCase.transform.position + summonedObj.transform.position - summonedObj.originPoint.position, Quaternion.identity);
-        SummonManager.Instance.lastSummonInstancied.owner = GameManager.Instance.currentPlayer;
-        SummonManager.Instance.lastSummonInstancied.element = elementCreated;
-        SummonManager.Instance.lastSummonInstancied.ChangeSpriteByPlayer();
-        SummonManager.Instance.lastSummonInstancied.GetComponentInChildren<SpriteRenderer>().color = new Color(1, 1, 1, 0.3f);
+        if (SummonManager.Instance.lastSummonInstancied == null)
+          {
+            SummonManager.Instance.lastSummonInstancied = (SummonData)Instantiate(summonedObj, hoveredCase.transform.position + summonedObj.transform.position - summonedObj.originPoint.position, Quaternion.identity);
+            SummonManager.Instance.lastSummonInstancied.owner = GameManager.Instance.currentPlayer;
+            SummonManager.Instance.lastSummonInstancied.element = elementCreated;
+            SummonManager.Instance.lastSummonInstancied.ChangeSpriteByPlayer();
+            SummonManager.Instance.lastSummonInstancied.GetComponentInChildren<SpriteRenderer>().color = new Color(1, 1, 1, 0.3f);
+          }
+        SummonManager.Instance.lastSummonInstancied.transform.position = hoveredCase.transform.position + SummonManager.Instance.lastSummonInstancied.transform.position - SummonManager.Instance.lastSummonInstancied.originPoint.position;
       }
-      SummonManager.Instance.lastSummonInstancied.transform.position = hoveredCase.transform.position + SummonManager.Instance.lastSummonInstancied.transform.position - SummonManager.Instance.lastSummonInstancied.originPoint.position;
-    }
   }
 
   public void ApplyEffect(GameObject objAfflicted)
@@ -316,75 +314,75 @@ public class SpellData : NetworkBehaviour
     if (pushValue != 0)
       ApplyPushEffect(objAfflicted);
     else
-    {
-      ApplyStatsEffect(objAfflicted);
-    }
+      {
+        ApplyStatsEffect(objAfflicted);
+      }
   }
+
   public void ApplyPushEffect(GameObject objAfflicted)
   {
     if (objAfflicted.GetComponent<PersoData>() != null)
-    {
-      PersoData persoAfflicted = objAfflicted.GetComponent<PersoData>();
-      CaseData caseAfflicted = persoAfflicted.persoCase;
-      if (animatorSpell != null)
-        FXManager.Instance.Show(animatorSpell, caseAfflicted.transform, SelectionManager.Instance.selectedPersonnage.persoDirection);
+      {
+        PersoData persoAfflicted = objAfflicted.GetComponent<PersoData>();
+        CaseData caseAfflicted = persoAfflicted.persoCase;
+        if (animatorSpell != null)
+          FXManager.Instance.Show(animatorSpell, caseAfflicted.transform, SelectionManager.Instance.selectedPersonnage.persoDirection);
 
-      EffectManager.Instance.Push(objAfflicted, caseAfflicted, pushValue, pushType, pushDirection);
-    }
+        EffectManager.Instance.Push(objAfflicted, caseAfflicted, pushValue, pushType, pushDirection);
+      }
 
     if (objAfflicted.GetComponent<BallonData>() != null)
-    {
-      BallonData ballonAfflicted = objAfflicted.GetComponent<BallonData>();
-      CaseData caseAfflicted = ballonAfflicted.ballonCase;
-      if (animatorSpell != null)
-        FXManager.Instance.Show(animatorSpell, caseAfflicted.transform, SelectionManager.Instance.selectedPersonnage.persoDirection);
+      {
+        BallonData ballonAfflicted = objAfflicted.GetComponent<BallonData>();
+        CaseData caseAfflicted = ballonAfflicted.ballonCase;
+        if (animatorSpell != null)
+          FXManager.Instance.Show(animatorSpell, caseAfflicted.transform, SelectionManager.Instance.selectedPersonnage.persoDirection);
 
-      EffectManager.Instance.Push(objAfflicted, caseAfflicted, pushValue, pushType, pushDirection);
-    }
+        EffectManager.Instance.Push(objAfflicted, caseAfflicted, pushValue, pushType, pushDirection);
+      }
 
     if (objAfflicted.GetComponent<SummonData>() != null)
-    {
-      SummonData summonAfflicted = objAfflicted.GetComponent<SummonData>();
-      CaseData caseAfflicted = summonAfflicted.caseActual;
-      if (animatorSpell != null)
-        FXManager.Instance.Show(animatorSpell, caseAfflicted.transform, SelectionManager.Instance.selectedPersonnage.persoDirection);
+      {
+        SummonData summonAfflicted = objAfflicted.GetComponent<SummonData>();
+        CaseData caseAfflicted = summonAfflicted.caseActual;
+        if (animatorSpell != null)
+          FXManager.Instance.Show(animatorSpell, caseAfflicted.transform, SelectionManager.Instance.selectedPersonnage.persoDirection);
 
-      EffectManager.Instance.Push(objAfflicted, caseAfflicted, pushValue, pushType, pushDirection);
-    }
+        EffectManager.Instance.Push(objAfflicted, caseAfflicted, pushValue, pushType, pushDirection);
+      }
   }
 
   public void ApplyStatsEffect(GameObject objAfflicted)
   {
     if (objAfflicted.GetComponent<PersoData>() != null)
-    {
-      PersoData persoAfflicted = objAfflicted.GetComponent<PersoData>();
-      CaseData caseAfflicted = persoAfflicted.persoCase;
-      if (animatorSpell != null)
-        FXManager.Instance.Show(animatorSpell, caseAfflicted.transform, SelectionManager.Instance.selectedPersonnage.persoDirection);
+      {
+        PersoData persoAfflicted = objAfflicted.GetComponent<PersoData>();
+        CaseData caseAfflicted = persoAfflicted.persoCase;
+        if (animatorSpell != null)
+          FXManager.Instance.Show(animatorSpell, caseAfflicted.transform, SelectionManager.Instance.selectedPersonnage.persoDirection);
 
-      if (persoAfflicted.owner != SelectionManager.Instance.selectedPersonnage.owner || !reverseDamageOnAlly)
-      {
-        EffectManager.Instance.ChangePR(persoAfflicted, -damagePR);
-        AfterFeedbackManager.Instance.PRText(damagePR, caseAfflicted.gameObject);
-        EffectManager.Instance.ChangePADebuff(persoAfflicted, -damagePA);
-        EffectManager.Instance.ChangePMDebuff(persoAfflicted, -damagePM);
+        if (persoAfflicted.owner != SelectionManager.Instance.selectedPersonnage.owner || !reverseDamageOnAlly)
+          {
+            EffectManager.Instance.ChangePR(persoAfflicted, -damagePR);
+            AfterFeedbackManager.Instance.PRText(damagePR, caseAfflicted.gameObject);
+            EffectManager.Instance.ChangePADebuff(persoAfflicted, -damagePA);
+            EffectManager.Instance.ChangePMDebuff(persoAfflicted, -damagePM);
+          } else if (persoAfflicted.owner == SelectionManager.Instance.selectedPersonnage.owner)
+          {
+            EffectManager.Instance.ChangePR(persoAfflicted, damagePR);
+            AfterFeedbackManager.Instance.PRText(damagePR, caseAfflicted.gameObject, true);
+            EffectManager.Instance.ChangePA(damagePA);
+            EffectManager.Instance.ChangePM(persoAfflicted, damagePM);
+          }
       }
-      else if (persoAfflicted.owner == SelectionManager.Instance.selectedPersonnage.owner)
-      {
-        EffectManager.Instance.ChangePR(persoAfflicted, damagePR);
-        AfterFeedbackManager.Instance.PRText(damagePR, caseAfflicted.gameObject, true);
-        EffectManager.Instance.ChangePA(persoAfflicted, damagePA);
-        EffectManager.Instance.ChangePM(persoAfflicted, damagePM);
-      }
-    }
     if (objAfflicted.GetComponent<SummonData>() != null)
-    {
-      SummonData summonAfflicted = objAfflicted.GetComponent<SummonData>();
-      CaseData caseAfflicted = summonAfflicted.caseActual;
-      if (animatorSpell != null)
-        FXManager.Instance.Show(animatorSpell, caseAfflicted.transform, SelectionManager.Instance.selectedPersonnage.persoDirection);
+      {
+        SummonData summonAfflicted = objAfflicted.GetComponent<SummonData>();
+        CaseData caseAfflicted = summonAfflicted.caseActual;
+        if (animatorSpell != null)
+          FXManager.Instance.Show(animatorSpell, caseAfflicted.transform, SelectionManager.Instance.selectedPersonnage.persoDirection);
 
-      EffectManager.Instance.ChangePR(summonAfflicted, -damagePR);
-    }
+        EffectManager.Instance.ChangePR(summonAfflicted, -damagePR);
+      }
   }
 }
