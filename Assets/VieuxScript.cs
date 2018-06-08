63692de880ef673f70d8501d15d7b793b5c6107c
Unknown <oncheonche@ymail.com>
2018-06-08 16:54:12 +0200

    Script de rodage

---
+++ Assets/VieuxScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VieuxScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    while (true)
+    {
+      int i = 0;
+      i++;
+    }
+	}
+}
