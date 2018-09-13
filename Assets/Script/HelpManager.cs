46ebd1bc8c348b2ecca5b87ca3afb818ca44c9fb
Archer01 <julienguenard01@gmail.com>
2018-09-13 18:25:36 +0200

    Tuto Intégration 1/x

--- Assets/Script/HelpManager.cs
+++ Assets/Script/HelpManager.cs
@@ -7,14 +7,28 @@ using UnityEngine.UI;
 public class HelpManager : MonoBehaviour {
 
     public static HelpManager Instance;
+
     Image helpScreen;
+    Image cross;
+    Image right;
+    Image left;
+    Image sumary;
+
     Vector3 cameraPos;
+
+    public List<Sprite> pageList;
+    int pageActual;
 
 	void Awake ()
     {
         Instance = this;
         helpScreen = GameObject.Find("HelpScreen").GetComponent<Image>();
+        cross = GameObject.Find("HelpScreen/Cross").GetComponent<Image>();
+        right = GameObject.Find("HelpScreen/Right").GetComponent<Image>();
+        left = GameObject.Find("HelpScreen/Left").GetComponent<Image>();
+        sumary = GameObject.Find("HelpScreen/Sumary").GetComponent<Image>();
         cameraPos = Camera.main.transform.position;
+        pageActual = 0;
         HideHelpScreen();
 	}
 
@@ -25,14 +39,41 @@ public class HelpManager : MonoBehaviour {
             HideHelpScreen();
             return;
         }
+        ChangeImageStatut(true);
         cameraPos = Camera.main.transform.position;
         Camera.main.transform.position = new Vector3 (600,600,600);
-        helpScreen.enabled = true;
+        
 	}
 
     public void HideHelpScreen()
     {
         Camera.main.transform.position = cameraPos;
-        helpScreen.enabled = false;
+        ChangeImageStatut(false);
+    }
+
+    public void NextPage(int increment)
+    {
+        pageActual += increment;
+
+        if (pageActual > pageList.Count-1)
+        {
+            pageActual = 0;
+        }
+
+        if (pageActual < 0)
+        {
+            pageActual = pageList.Count-1;
+        }
+
+        helpScreen.sprite = pageList[pageActual];
+    }
+
+    void ChangeImageStatut(bool isEnabled)
+    {
+        helpScreen.enabled = isEnabled;
+        cross.enabled = isEnabled;
+        right.enabled = isEnabled;
+        left.enabled = isEnabled;
+        sumary.enabled = isEnabled;
     }
 }
